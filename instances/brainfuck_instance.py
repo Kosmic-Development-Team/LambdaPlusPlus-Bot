ec7bf9d85407f32d564b7a09ceeb854ae2b19c65
KosmicCo <cruzbarnum@gmail.com>
2017-08-25 17:11:22 -0700

    Started implementing the Brainfuck Instance and integration with the bot. making sure that config, bot, and interpreter are set for its full implementation (Hint: they're not at the current moment).

--- instances/brainfuck_instance.py
+++ instances/brainfuck_instance.py
@@ -9,13 +9,13 @@ class BrainfuckInstance(ProjectInstance):
         self.use_16bit = self.bot.config.use_16bit
         self.input_mode = self.bot.config.input_mode
         self.input_hex = self.bot.config.use_hex
-        self.output_hex = False             #make a config option *********************************************
+        self.output_hex = self.bot.config.use_hex  # make a config option *********************************************
         self.use_extra = self.bot.config.use_extra_chars
         self.use_meta = self.bot.config.use_meta_chars
-        self.max_iterations = 1024          #make a config option *********************************************
+        self.max_iterations = 1024                 # make a config option *********************************************
         self.return_fill = self.bot.config.return_fill
 
-        self.bf_interpreter = BrainfuckInterpreter(self.use_16bit, self.input_mode, self.use_hex, self.use_extra, self.use_meta, self.max_iterations)
+        self.bf_interpreter = BrainfuckInterpreter(self.use_16bit, self.input_mode, self.use_extra, self.use_meta, self.max_iterations)
 
     def handle_command(self, message, prefix, command, rest):
         pass
